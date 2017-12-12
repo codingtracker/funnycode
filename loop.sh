eb5065e49769fb8082f1f74bb913fede973d1972
codingtracker <chen.winfield.ye@gmail.com>
2017-12-12 10:40:54 -0500

    add screen to make dog does not ruin your command session

--- loop.sh
+++ loop.sh
@@ -4,9 +4,9 @@ i=2
 while [ $sig -ne 1 ]
 do 
 	if [ $i -le $((`tput lines`-2)) ]; then 
-		while [ $i -le $((`tput lines`-2)) ] ; do /bin/bash ./dogrun.sh $i; let i++; done
+		while [ $i -le $((`tput lines`-2)) ] ; do screen -m /bin/bash ./dogrun.sh $i; let i++; done
 	else
-		while [ $i -gt 3 ] ; do /bin/bash ./dogrun.sh $i; let i--;  done
+		while [ $i -gt 3 ] ; do screen -m /bin/bash ./dogrun.sh $i; let i--;  done
 	fi
 done	
 
