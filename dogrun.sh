2534d8cdf57c49e27ec3d70dbf8d897550b4759b
codingtracker <chen.winfield.ye@gmail.com>
2017-12-12 10:29:12 -0500

    rename filename, adding flashing caption at line 1, lease and set off the dog running to fetch from line 2

--- dogrun.sh
+++ dogrun.sh
@@ -2,29 +2,35 @@ LCyan='\033[1;36m'
 Yellow='\033[1;33m'
 NC='\033[0m'
 
-while [ $1 -eq 1 ]
-do
-        clear
-        echo ""
-        printf " ${LCyan}Now fetch...${NC}"
-        echo ""
-        sleep 3
-        break
-done
+if [ $1 -eq 2 ]; then
+	clear
+	tput cup 1 2; printf " ${LCyan}Now fetch...${NC}"
+	sleep 5
+fi
 
-        for((pst=0;pst<$((`tput cols`-7));pst=pst+120)) {
+caption () {
+        echo ""
+        tput cup 1 2; printf " ${LCyan} Feeeetching...${NC}"
+        echo ""
+}
+
+        for((pst=0;pst<$((`tput cols`-7));pst=pst+10)) {
 		
                 nxt=$(($1+1))
 #               echo $1
 #               echo $nxt
-                clear
                 tput civis
+		caption
+		sleep 1
+                clear
                 tput cup $1 $pst; printf "${Yellow}~(__/=${NC}"
                 tput cup $nxt $pst; printf "${Yellow} \" \"${NC}"
                 sleep 1
         }
 
-        for((pst=$((`tput cols`-7));pst>0;pst=pst-120)) {
+        for((pst=$((`tput cols`-7));pst>0;pst=pst-10)) {
+		caption
+		sleep 1
                 clear
                 tput cup $1 $pst; printf "${Yellow}=\__)~${NC}"
                 tput cup $nxt $pst; printf "${Yellow} \" \"${NC}"
--- loop.sh
+++ loop.sh
@@ -1,12 +1,12 @@
 sig=0
-i=63
+i=2
 
 while [ $sig -ne 1 ]
 do 
 	if [ $i -le $((`tput lines`-2)) ]; then 
-		while [ $i -le $((`tput lines`-2)) ] ; do /bin/bash ./echo3.sh $i; let i++; done
+		while [ $i -le $((`tput lines`-2)) ] ; do /bin/bash ./dogrun.sh $i; let i++; done
 	else
-		while [ $i -gt 3 ] ; do /bin/bash ./echo3.sh $i; let i--; echo $i; done
+		while [ $i -gt 3 ] ; do /bin/bash ./dogrun.sh $i; let i--;  done
 	fi
 done	
 
